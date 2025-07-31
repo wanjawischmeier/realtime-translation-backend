9b5d7a45a4c18eea8b53c2aeb6f29fe955c52c54
whoami <schmusi@systemli.org>
2025-07-31 13:54:37 +0200

    Added Event Class to simplify getting information (And for Room Manager)

--- src/pretalx_api_wrapper.py
+++ src/pretalx_api_wrapper.py
@@ -4,11 +4,21 @@ import dateutil.parser
 import pytz
 import requests
 
-
 class Track:
     def __init__(self, name:str, color:str):
         self.name = name
         self.color = color
+
+class Event:
+    def __init__(self, code:str, title: str, track:str, location:str, url:str, description:str, organizer:str, do_not_record:bool):
+        self.id = code
+        self.title = title
+        self.track = track
+        self.location = location
+        self.url = url
+        self.do_not_record = do_not_record
+        self.organizer = organizer
+        self.description = description
 
 class Conference:
     def __init__(self, title: str, start: date, end: date, days: int, url: str, timezone: pytz.tzinfo,
@@ -62,6 +72,8 @@ class PretalxAPI:
                         now_start_delta  = (datetime.fromisoformat(fake_now) - dateutil.parser.isoparse(event['date']))
                     # Filter events to only include the ongoing events and those that start in less than 30 minutes
                     if now_start_delta.total_seconds() <= (720 * 60) and timedelta(minutes=-31) < now_start_delta < duration:
+                        eve=Event(event['code'], event['title'], event['track'], event['room'], event['url'], event['description'], event['persons'][0]['name'], event['do_not_record'])
+                        print(eve.__dict__)
                         self.ongoing_events.append(event)
         self.ongoing_events.sort(key=lambda e: dateutil.parser.isoparse(e['date'])) # Sorts list by date
 
